3add772e5f5fa7d16f910ad8152a8d2fc7bd2119
klakiotis <klimislak7@gmail.com>
2015-12-15 11:25:02 +0200

    my x-mass tree

--- ChristmasTree.py
+++ ChristmasTree.py
@@ -1,10 +1,15 @@
-i = 0
-blank=' '
-while i < 10:
- print((" " * (10 - i)) + ("*" * (i * 2 + 1)))
+print("As ftiaksoume ena x-mass tree. Dwse diastaseis dentrou")
+ypsos_dentrou=input("Dwse to ypsos tou dentrou:")
+platos_kormou=input("Dwse to platos tou kormou tou dentrou: ")
+ypsos_kormou=input("Dwse to ypsos tou kormou tou dentrou: ")
+n=ypsos_dentrou
+x=ypsos_kormou 
+z=platos_kormou
+i=0
+while i < n:
+ print((" " * (n - i)) + ("*" * (i * 2 + 1)))
  i = i + 1
-i = 0
-j = 1
-while j < 4:
- print(' ' * 9 + '*' * 2 + ' ' * 9)
- j = j + 1
+j = 0
+while j < x:
+ print(' ' * int((2*n-1)/2) + '*' * z + ' ' * ((2*n-1)/2))
+ j = j + 1 
